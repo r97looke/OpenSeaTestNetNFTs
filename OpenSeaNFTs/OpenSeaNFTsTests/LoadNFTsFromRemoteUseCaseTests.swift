c72a2c7aba36ed5321e05bcd2309cd8fbe9f7349
Shun Lung Chen <r97looke@gmail.com>
2023-09-01 16:42:45 +0800

    RemoteNFTsLoader delivers error on Non 200 HTTP response

--- OpenSeaNFTs/OpenSeaNFTsTests/LoadNFTsFromRemoteUseCaseTests.swift
+++ OpenSeaNFTs/OpenSeaNFTsTests/LoadNFTsFromRemoteUseCaseTests.swift
@@ -31,12 +31,19 @@ class RemoteNFTsLoader: NFTsLoader {
         self.client = client
     }
     
+    private enum LoadError: Swift.Error {
+        case connectivity
+        case invalidData
+    }
+    
     func load(next: String? = nil, completion: @escaping (LoadResult) -> Void) {
         client.get(from: url) { result in
             switch result {
             case let .failure(error):
-                completion(.failure(error))
+                completion(.failure(LoadError.connectivity))
                 
+            case let .success((data, httpURLResponse)):
+                completion(.failure(LoadError.invalidData))
             default:
                 break
             }
@@ -88,6 +95,31 @@ final class LoadNFTsFromRemoteUseCaseTests: XCTestCase {
         XCTAssertNotNil(receivedError)
     }
     
+    func test_load_deliversErrorOnNon200HTTPResponse() {
+        let url = anyURL()
+        let client = HTTPClientSpy()
+        let sut = RemoteNFTsLoader(url: url, client: client)
+        
+        let exp = expectation(description: "Wait load to complete")
+        var receivedError: Error?
+        sut.load { result in
+            switch result {
+            case let .failure(error):
+                receivedError = error
+                
+            default:
+                XCTFail("Expect error, got \(result) instead")
+            }
+            
+            exp.fulfill()
+        }
+        
+        client.completeWith(statusCode: 199, data: Data())
+        
+        wait(for: [exp], timeout: 1.0)
+        XCTAssertNotNil(receivedError)
+    }
+    
     // MARK: Helpers
     private class HTTPClientSpy: HTTPClient {
         var requestURLs = [URL]()
@@ -100,6 +132,10 @@ final class LoadNFTsFromRemoteUseCaseTests: XCTestCase {
         
         func completeWith(error: Error, at index: Int = 0) {
             completions[index](.failure(error))
+        }
+        
+        func completeWith(statusCode: Int, data: Data, at index: Int = 0) {
+            completions[index](.success((data, HTTPURLResponse(url: requestURLs[index], statusCode: statusCode, httpVersion: nil, headerFields: nil)!)))
         }
     }
     
