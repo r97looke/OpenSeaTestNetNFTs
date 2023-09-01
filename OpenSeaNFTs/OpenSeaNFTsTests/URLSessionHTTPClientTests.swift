311be989c5a3ffc2b6b09286f912a7601d08a910
Shun Lung Chen <r97looke@gmail.com>
2023-09-01 22:56:26 +0800

    URLSessionHTTPClient get does request from URL

--- OpenSeaNFTs/OpenSeaNFTsTests/URLSessionHTTPClientTests.swift
+++ OpenSeaNFTs/OpenSeaNFTsTests/URLSessionHTTPClientTests.swift
@@ -9,36 +9,115 @@ import XCTest
 import OpenSeaNFTs
 
 class URLSessionHTTPClient: HTTPClient {
+    private struct InvalidDataResponseErrorCombination: Swift.Error { }
+    
+    let session: URLSession
+    
+    init(session: URLSession = .shared) {
+        self.session = session
+    }
     
     func get(from url: URL, completion: @escaping (GETResult) -> Void) {
-        
+        session.dataTask(with: URLRequest(url: url)) { data, response, error in
+            completion(.failure(InvalidDataResponseErrorCombination()))
+        }.resume()
     }
 }
 
 final class URLSessionHTTPClientTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        
+        URLProtocolStub.startInterceptRequests()
+    }
+    
+    override func tearDown() {
+        super.tearDown()
+        
+        URLProtocolStub.stopInterceptRequests()
+    }
 
-//    func test_getFromURL_doesGETRequestWithURL() {
-//        let url = anyURL()
-//        let sut = makeSUT()
-//        let exp = expectation(description: "Wait for complete")
-//        
-//        URLProtocolStub.requestObserver = { request in
-//            XCTAssertEqual(request.httpMethod, "GET")
-//            XCTAssertEqual(request.url, url)
-//            
-//            exp.fulfill()
-//        }
-//        
-//        sut.get(from: url) { _ in }
-//        
-//        wait(for: [exp], timeout: 1.0)
-//    }
-//    
-//    // MARK: Helpers
-//    private func makeSUT() -> URLSessionHTTPClient {
-//        let sut = URLSessionHTTPClient()
-//        trackForMemoryLeaks(sut)
-//        return sut
-//    }
+    func test_getFromURL_doesGETRequestWithURL() {
+        let url = anyURL()
+        let sut = makeSUT()
+        let exp = expectation(description: "Wait for complete")
+        
+        URLProtocolStub.requestObserver = { request in
+            XCTAssertEqual(request.httpMethod, "GET")
+            XCTAssertEqual(request.url, url)
+            
+            exp.fulfill()
+        }
+        
+        sut.get(from: url) { _ in }
+        
+        wait(for: [exp], timeout: 1.0)
+    }
+    
+    // MARK: Helpers
+    private func makeSUT() -> URLSessionHTTPClient {
+        let sut = URLSessionHTTPClient()
+        trackForMemoryLeaks(sut)
+        return sut
+    }
+    
+    private class URLProtocolStub: URLProtocol {
+        private struct Stub {
+            let data: Data?
+            let response: URLResponse?
+            let error: Error?
+        }
+        
+        static func startInterceptRequests() {
+            URLProtocol.registerClass(URLProtocolStub.self)
+        }
+        
+        static func stopInterceptRequests() {
+            URLProtocol.unregisterClass(URLProtocolStub.self)
+            stub = nil
+            requestObserver = nil
+        }
+        
+        static var requestObserver: ((URLRequest) -> Void)?
+        
+        private static var stub: Stub?
+        
+        static func stub(data: Data?, response: URLResponse?, error: Error?) {
+            stub = Stub(data: data, response: response, error: error)
+        }
+        
+        override class func canInit(with request: URLRequest) -> Bool {
+            return true
+        }
+        
+        override class func canonicalRequest(for request: URLRequest) -> URLRequest {
+            return request
+        }
+        
+        override func startLoading() {
+            if let observer = URLProtocolStub.requestObserver {
+                observer(request)
+                client?.urlProtocolDidFinishLoading(self)
+                return
+            }
+            
+            if let error = URLProtocolStub.stub?.error {
+                client?.urlProtocol(self, didFailWithError: error)
+            }
+            
+            if let data = URLProtocolStub.stub?.data {
+                client?.urlProtocol(self, didLoad: data)
+            }
+            
+            if let response = URLProtocolStub.stub?.response {
+                client?.urlProtocol(self, didReceive: response, cacheStoragePolicy: .notAllowed)
+            }
+            
+            client?.urlProtocolDidFinishLoading(self)
+        }
+        
+        override func stopLoading() { }
+    }
 
 }
