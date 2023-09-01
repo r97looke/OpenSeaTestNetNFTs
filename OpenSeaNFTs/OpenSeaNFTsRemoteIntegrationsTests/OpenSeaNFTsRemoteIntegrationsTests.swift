8662b6c736c1ae8a3b5b9cf451011a6e77d9639b
Shun Lung Chen <r97looke@gmail.com>
2023-09-01 23:20:30 +0800

    Add OpenSeaNFTsRemoteIntegrationsTests

---
+++ OpenSeaNFTs/OpenSeaNFTsRemoteIntegrationsTests/OpenSeaNFTsRemoteIntegrationsTests.swift
@@ -0,0 +1,59 @@
+//
+//  OpenSeaNFTsRemoteIntegrationsTests.swift
+//  OpenSeaNFTsRemoteIntegrationsTests
+//
+//  Created by Shun Lung Chen on 2023/9/1.
+//
+
+import XCTest
+import OpenSeaNFTs
+
+final class OpenSeaNFTsRemoteIntegrationsTests: XCTestCase {
+
+    func test_load_NFTs() {
+        let url = testEndpointURL()
+        let session = URLSession(configuration: .ephemeral)
+        let client = URLSessionHTTPClient(session: session)
+        let loader = RemoteNFTsLoader(url: url, client: client)
+        
+        let exp = expectation(description: "Wait loader to complete")
+        loader.load { result in
+            switch result {
+            case let .success(nfts):
+                XCTAssertEqual(nfts.count, 20)
+                
+            default:
+                XCTFail("Expect success, got \(result) instead")
+            }
+            
+            exp.fulfill()
+        }
+        
+        wait(for: [exp], timeout: 5.0)
+    }
+    
+    // MARK: Helpers
+    private func chain() -> String {
+        return "goerli"
+    }
+    
+    private func account() -> String {
+        return "0x85fD692D2a075908079261F5E351e7fE0267dB02"
+    }
+    
+    private func testBaseEndpointURL() -> URL {
+        return URL(string: "https://testnets-api.opensea.io/v2")!
+    }
+    
+    private func testEndpointURL() -> URL {
+        let queryItem = [URLQueryItem(name: "limit", value: "20")]
+        var url = testBaseEndpointURL()
+        return url.appendingPathComponent("chain")
+            .appendingPathComponent(chain())
+            .appendingPathComponent("account")
+            .appendingPathComponent(account())
+            .appendingPathComponent("nfts")
+            .appending(queryItems: queryItem)
+    }
+
+}
